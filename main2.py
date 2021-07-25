4b5d2414bbd3da006444171c705e61d0babf70e9
mohammadmubaslat <mubaslat2044@gmail.com>
2021-07-25 11:46:25 +0300

    4

--- main2.py
+++ main2.py
@@ -20,11 +20,7 @@ label = Label(  my_tk,image=img)
 label.place(x=0, y=0)
 
 
-def friend_button_fun():
-    newWindow = Toplevel(my_tk)
-    newWindow.geometry("750x500")
-    Label(newWindow, image=img).pack()
-
+def easy_button_fun():
 
 
 
@@ -312,7 +308,7 @@ def friend_button_fun():
                     screen.blit(label, (40, 10))
                     game_over = True
 
-            #    print_board(board)
+                print_board(board)
                 #   draw_board(board)
 
                 turn += 1
@@ -321,6 +317,165 @@ def friend_button_fun():
         if game_over:
             pygame.time.wait(3000)
 
+def friend_button_fun():
+    newWindow = Toplevel(my_tk)
+    newWindow.geometry("750x500")
+    Label(newWindow, image=img).pack()
+
+    import numpy as np
+    import pygame
+    import sys
+    import math
+
+    BLUE = (0, 0, 255)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    YELLOW = (255, 255, 0)
+
+    ROW_COUNT = 6
+    COLUMN_COUNT = 7
+
+    def create_board():
+        board = np.zeros((ROW_COUNT, COLUMN_COUNT))
+        return board
+
+    def drop_piece(board, row, col, piece):
+        board[row][col] = piece
+
+    def is_valid_location(board, col):
+        return board[ROW_COUNT - 1][col] == 0
+
+    def get_next_open_row(board, col):
+        for r in range(ROW_COUNT):
+            if board[r][col] == 0:
+                return r
+
+    def print_board(board):
+        print(np.flip(board, 0))
+
+    def winning_move(board, piece):
+        # Check horizontal locations for win
+        for c in range(COLUMN_COUNT - 3):
+            for r in range(ROW_COUNT):
+                if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][
+                    c + 3] == piece:
+                    return True
+
+        # Check vertical locations for win
+        for c in range(COLUMN_COUNT):
+            for r in range(ROW_COUNT - 3):
+                if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][
+                    c] == piece:
+                    return True
+
+        # Check positively sloped diaganols
+        for c in range(COLUMN_COUNT - 3):
+            for r in range(ROW_COUNT - 3):
+                if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and \
+                        board[r + 3][c + 3] == piece:
+                    return True
+
+        # Check negatively sloped diaganols
+        for c in range(COLUMN_COUNT - 3):
+            for r in range(3, ROW_COUNT):
+                if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and \
+                        board[r - 3][c + 3] == piece:
+                    return True
+
+    def draw_board(board):
+        for c in range(COLUMN_COUNT):
+            for r in range(ROW_COUNT):
+                pygame.draw.rect(screen, BLUE, (c * SQUARESIZE, r * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE))
+                pygame.draw.circle(screen, BLACK, (
+                int(c * SQUARESIZE + SQUARESIZE / 2), int(r * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+
+        for c in range(COLUMN_COUNT):
+            for r in range(ROW_COUNT):
+                if board[r][c] == 1:
+                    pygame.draw.circle(screen, RED, (
+                    int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+                elif board[r][c] == 2:
+                    pygame.draw.circle(screen, YELLOW, (
+                    int(c * SQUARESIZE + SQUARESIZE / 2), height - int(r * SQUARESIZE + SQUARESIZE / 2)), RADIUS)
+        pygame.display.update()
+
+    board = create_board()
+    print_board(board)
+    game_over = False
+    turn = 0
+
+    pygame.init()
+
+    SQUARESIZE = 100
+
+    width = COLUMN_COUNT * SQUARESIZE
+    height = (ROW_COUNT + 1) * SQUARESIZE
+
+    size = (width, height)
+
+    RADIUS = int(SQUARESIZE / 2 - 5)
+
+    screen = pygame.display.set_mode(size)
+    draw_board(board)
+    pygame.display.update()
+
+    myfont = pygame.font.SysFont("monospace", 75)
+
+    while not game_over:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+                posx = event.pos[0]
+                if turn == 0:
+                    pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE / 2)), RADIUS)
+                else:
+                    pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE / 2)), RADIUS)
+            pygame.display.update()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+                # print(event.pos)
+                # Ask for Player 1 Input
+                if turn == 0:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx / SQUARESIZE))
+
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        drop_piece(board, row, col, 1)
+
+                        if winning_move(board, 1):
+                            label = myfont.render("Player 1 wins!!", 1, RED)
+                            screen.blit(label, (40, 10))
+                            game_over = True
+
+
+                # # Ask for Player 2 Input
+                else:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx / SQUARESIZE))
+
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        drop_piece(board, row, col, 2)
+
+                        if winning_move(board, 2):
+                            label = myfont.render("Player 2 wins!!", 1, YELLOW)
+                            screen.blit(label, (40, 10))
+                            game_over = True
+
+                print_board(board)
+                draw_board(board)
+                turn += 1
+                turn = turn % 2
+                if game_over:
+                    pygame.time.wait(3000)
+
+
 
 
 def computer_button_fun():
@@ -328,17 +483,20 @@ def computer_button_fun():
     newWindow2.geometry("750x500")
     Label(newWindow2, image=img).pack()
 
-    easy_button = Button(newWindow2, text='Easy',height=2 , width=20 )
+    easy_button = Button(newWindow2, text='Easy',height=2 , width=20 ,  command= easy_button_fun)
     easy_button.pack()
     easy_button.place(x=290, y=220)
 
-    med_button = Button(newWindow2, text='Medium',height=2 , width=20 )
+    med_button = Button(newWindow2, text='Normal',height=2 , width=20 )
     med_button.pack()
     med_button.place(x=290, y=290)
 
     hard_button = Button(newWindow2, text='Hard', height=2, width=20 )
     hard_button.pack()
     hard_button.place(x=290, y=360)
+
+
+
 
 
 friend_button = Button(my_tk, text='Play with friend',height=2 , width=30, command= friend_button_fun)
